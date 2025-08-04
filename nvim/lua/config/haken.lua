cd9c501e593ea543365d0bee197ce3ba281c2965
lineick <75929322+lineick@users.noreply.github.com>
2025-08-04 15:31:19 +0200

    small change

--- nvim/lua/config/haken.lua
+++ nvim/lua/config/haken.lua
@@ -1,17 +1,33 @@
 -- if true then return {} end
--- stack is recommended for haken.nvim, easier to follow and more intuitive
 -- vim.o.jumpoptions = "stack"
 
 local haken = require("haken")
 
 haken.setup({
-  clear_jumplists = true,
-  column_sensitive = true,
-  clear_on_new_window = false,
+  clear_jumps_on_startup = true,
+  column_sensitive = false,
+  clear_jumps_on_new_window = false,
+  silent = false,
 })
 
+-- using jumpoptions=stack is not recommended with add_haken(false), can lead to confusing behavior
 vim.keymap.set('n', '<BS>', haken.add_haken, {
   desc = "Add haken",
   silent = true,
 })
 
+vim.keymap.set('n', '<leader><BS>', haken.prune_jumps, {
+  desc = "Prune jumps to most recent jump",
+  silent = true,
+})
+
+-- vim.keymap.set('n', '<C-o>', function()
+--   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-o>', true, false, true), 'nx', false)
+--   haken.set_recent_haken()
+-- end, { noremap = true, silent = true })
+--
+-- vim.keymap.set('n', '<C-i>', function()
+--   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-i>', true, false, true), 'nx', false)
+--   haken.set_recent_haken()
+-- end, { noremap = true, silent = true })
+--
